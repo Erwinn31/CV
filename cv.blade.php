7f8b4f6b8c30d939a4da292a8e7a7801cab5f6ea
Erwinn31 <122732675+Erwinn31@users.noreply.github.com>
2023-03-22 23:06:00 +0700

    Add files via upload

---
+++ cv.blade.php
@@ -0,0 +1,135 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title> Erwin_2231061_CV </title>
+        <link rel="stylesheet" href="{{ asset('css/cv.css') }}">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css" integrity="sha512-5A8nwdMOWrSz20fDsjczgUidUBR8liPYU+WymTZP1lmY9G6Oc7HlZv156XqnsgNUzTyMefFTcsFH/tnJE/+xBg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    </head>
+    <body>
+        <div class="container">
+            <div class="left_Side">
+                <div class="profileText">
+                    <div class="img8x"> 
+                        <img src="images/pasfoto.jpeg">
+                    </div>
+                    <h2>Erwin<br><span>System Information</span></h2>
+                </div>
+
+                <div class="contactInfo">
+                    <h3 class="title">Contact Info</h3>
+                    <ul>
+                        <li>
+                            <span class="icon"><i class="fa fa-phone" aria-hidden="true"></i></span>
+                            <span class="text">+62 853-6361-8415</span>
+                        </li>
+                        <li>
+                            <span class="icon"><i class="fa fa-envelope-o" aria-hidden="true"></i></span>
+                            <span class="text">erwinerwinnn31@gmail.com</span>
+                        </li>
+                        <li>
+                            <span class="icon"><i class="fa fa-map-marker" aria-hidden="true"></i></span>
+                            <span class="text">Batam, Indonesia</span>
+                        </li>
+                    </ul>
+                </div>
+
+                <div class="contactInfo education">
+                    <h3 class="title">Education</h3>
+                    <ul>
+                        <li>
+                            <h5>2022 - 2026</h5>
+                            <h4>System Information</h4>
+                            <h4>Universitas Internasional Batam</h4>
+                        </li>
+                        <li>
+                            <h5>2019 - 2022</h5>
+                            <h4>Mathematics and Science</h4>
+                            <h4>SMA Negeri 1 Tanjungpinang</h4>
+                        </li>
+                    </ul>
+                </div>
+
+                <div class="contactInfo language">
+                    <h3 class="title">Language</h3>
+                    <ul>
+                        <li>
+                            <span class="text">Indonesia</span>
+                            <span class="percent">
+                                <div style="width: 95%"></div>
+                            </span>
+                        </li>
+                        <li>
+                            <span class="text">English</span>
+                            <span class="percent">
+                            <div style="width: 60%"></div>
+                            </span>
+                        </li>
+                        <li>
+                            <span class="text">Mandarin</span>
+                            <span class="percent">
+                            <div style="width: 80%"></div>
+                            </span>
+                        </li>
+                    </ul>
+                </div>
+                
+            </div>
+
+            <div class="right_Side">
+                <div class="about">
+                    <h2 class="title2">Profile</h2>
+                    <p>
+                    Saya seorang lulusan SMAN Negeri 1 Tanjungpinang yang sedang menempuh pendidikan Sarjana pada Universitas Internasional Batam. Saya memiliki kemampuan berbicara dengan klien serta dapat bekerja secara mandiri maupun dalam tim. Saya  memiliki rasa ingin tahu yang besar dan selalu mencari peluang untuk belajar hal-hal baru dan meningkatkan keterampilan saya.<br><br>
+                    Saya pernah bekerja pada Perusahaan yang bergerak dibidang Security System, dimana perusahaan tersebut menjual produk seperti CCTV, Access Control, Alarm, Access Point, Switch, serta PABX                </p>
+                </div>
+                <div class="about">
+                    <h2 class="title2">Experience</h2>
+                    <div class="box">
+                    <div class="year_company">
+                        <h5>2022 - Present</h5>
+                        <h5>PT. FOCUS CENTRAL SECURINDO</h5>
+                    </div>
+                    <div class="text">
+                        <h4>Admin Gudang</h4>
+                        <p>Melakukan pengawasan terhadap stok barang di gudang, melakukan input data stok dan mengatur persediaan barang.<br><br>Mengolah data penjualan, stok barang, dan pelanggan secara akurat dan teratur menggunakan GF-Akuntansi</p>
+                        <br>
+                        <h4>Admin Sales</h4>
+                        <p>Melayani pelanggan dengan baik, mulai dari menerima penawaran, pemesanan, pengiriman, hingga penyelesaian keluhan pelanggan.<br><br>Membuat dan mengirimkan penawaran harga kepada pelanggan sesuai dengan permintaan.</p>
+                    </div>
+                </div>
+                </div>
+
+                <div class="about skills">
+                    <h2 class="title2">Skills</h2>
+                    <div class="box">
+                        <h4>Microsoft Word</h4>
+                        <div class="percent">
+                            <div style="width: 95%"></div>
+                        </div>
+                    </div>
+                    <div class="box">
+                        <h4>Microsoft Excel</h4>
+                        <div class="percent">
+                        <div style="width: 55%"></div>
+                        </div>
+                    </div>
+                    <div class="box">
+                        <h4>GF-Akuntansi</h4>
+                        <div class="percent">
+                        <div style="width: 65%"></div>
+                        </div>
+                    </div>
+                    <div class="box">
+                        <h4>Public Speaking</h4>
+                        <div class="percent">
+                        <div style="width: 80%"></div>
+                        </div>
+                    </div>
+                </div>
+
+
+            </div>
+        </div>
+    </body>
+</html>
